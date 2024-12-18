655197bd5aca021147596b0b17be65c206aacbf3
mpeter92 <mpeter92@yahoo.co.uk>
2024-12-18 11:15:44 +0000

    Update Runbook.ps1

--- Email admins last sign in report/Runbook.ps1
+++ Email admins last sign in report/Runbook.ps1
@@ -20,26 +20,32 @@ $arrOutput = [System.Collections.Generic.List[Object]]::new()
 
 # Loop through each user and store their data in the variable arrOutput
 Foreach ($user in Get-MgUser -All -Select id,userPrincipalName,displayName,accountEnabled,onPremisesSyncEnabled,createdDateTime,signInActivity) {
-    $lastsignin = ($user.signInActivity).lastSignInDateTime
+    $lastsignin = $null
+    if ($user.signInActivity -ne $null) {
+        $lastsignin = $user.signInActivity.lastSignInDateTime
+    }
     $ObjUsers = New-Object PSObject
     $ObjUsers | Add-Member NoteProperty -Name "Object ID" -Value $user.id
     $ObjUsers | Add-Member NoteProperty -Name "Display Name" -Value $user.displayName
     $ObjUsers | Add-Member NoteProperty -Name "User Principal Name" -Value $user.userPrincipalName
     $ObjUsers | Add-Member NoteProperty -Name "Account Enabled" -Value ($user.accountEnabled -ne $null)
     $ObjUsers | Add-Member NoteProperty -Name "onPremisesSyncEnabled" -Value ($user.onPremisesSyncEnabled -ne $null)
-    $ObjUsers | Add-Member NoteProperty -Name "Created DateTime (UTC)" -Value $user.createdDateTime
-    $ObjUsers | Add-Member NoteProperty -Name "Last Success Signin (UTC)" -Value ($user.signInActivity).lastSignInDateTime
-    $arrOutput.Add($ObjUsers) 
+    $ObjUsers | Add-Member NoteProperty -Name "Created DateTime (UTC)" -Value ($user.createdDateTime.ToString("dd-MM-yyyyTHH:mm:ssZ"))
+    if ($lastsignin -ne $null) {
+        $ObjUsers | Add-Member NoteProperty -Name "Last Success Signin (UTC)" -Value ($lastsignin.ToString("dd-MM-yyyyTHH:mm:ssZ"))
+    } else {
+        $ObjUsers | Add-Member NoteProperty -Name "Last Success Signin (UTC)" -Value "N/A"
     }
+    $arrOutput.Add($ObjUsers) 
+}
 
 # Get the current date in the desired format
 $currentDate = (Get-Date).ToString('dd-MM-yyyy')
 
 # Convert the results to CSV format in memory
-$csvContent = $arrOutput | Sort-Object UserPrincipalName, LastLogin | ConvertTo-Csv -NoTypeInformation -Delimiter ',' | Out-String
+$csvContent = $arrOutput | Sort-Object UserPrincipalName, "Last Success Signin (UTC)" | ConvertTo-Csv -NoTypeInformation -Delimiter ',' | Out-String
 $csvBytes = [System.Text.Encoding]::UTF8.GetBytes($csvContent)
 $csvBase64 = [System.Convert]::ToBase64String($csvBytes)
-
 
 # Email script
 $apiquery = "https://graph.microsoft.com/v1.0/users/$mailbox/sendMail"
@@ -51,7 +57,7 @@ $emailBody = @{
             content = "Sign in logs report."
         }
         toRecipients = @(@{ emailAddress = @{ address = $recipient } })
-        ccRecipients = @(@{ emailAddress = @{ address = "$recipientCC  } })
+        ccRecipients = @(@{ emailAddress = @{ address = $recipientCC } })
         attachments = @(@{
             '@odata.type' = '#microsoft.graph.fileAttachment'
             name = "SignInReport_$currentDate.csv"
